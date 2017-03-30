adcf061082f2cac9429e4165108e9b933689e2b0
Zachary Sang <zacharysang@gmail.com>
2017-03-30 09:10:52 -0400

    Added onmousedown and onmouseup reactions to showButton. Might need to initialize events more formally.

--- main.js
+++ main.js
@@ -8,5 +8,7 @@ function appendShowButton(passField){
   var showButton = document.createElement("button");
   showButton.setAttribute("value","Show Password");
   showButton.setAttribute("id","show_pass_show_button");
+  showButton.setAttribute("onmousedown","function(downEvent){downEvent.target.setAttribute("type","text");}");
+  showButton.setAttribute("onmouseup","function(upEvent){upEvent.target.setAttribute("type","password");}");
   passField.parentNode.insertBefore(showButton,passField.nextSibling);
 }
