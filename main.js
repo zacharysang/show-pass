d036fe1073974339e49a76910558bcbc77deb40d
Zachary Sang <zacharysang@gmail.com>
2017-04-11 09:52:17 -0400

    Changed trigger from button to hover event
    Removed the need for a button. User only needs to hover over the password field.

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 var passFields = document.getElementsBySelector'input[type="password"]');
 
-for(var i = 0 ; i < passField.length; i++){
-  appendShowButton(passFields[i]);
+passFields.forEach(addListeners);
+
+//make password visible on hover
+function addListeners(element){
+  element.addEventListener('mouseover',function(ev){
+    ev.target.setAttribute('type','text');
+  });
+  
+  //change it back on hover out
+  element.addEventListener('mouseout',function(ev){
+    ev.target.setAttribute('type','password')
+  });
 }
 
-function appendShowButton(passField){
-  var showButton = document.createElement("button");
-  showButton.setAttribute("value","Show Password");
-  showButton.setAttribute("id","show_pass_show_button");
-  showButton.setAttribute("onmousedown","function(downEvent){downEvent.target.setAttribute("type","text");}");
-  showButton.setAttribute("onmouseup","function(upEvent){upEvent.target.setAttribute("type","password");}");
-  passField.parentNode.insertBefore(showButton,passField.nextSibling);
-}
