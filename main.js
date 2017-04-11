c5f36aac69bc7fbb94d729082e4c7b538060dc73
Zachary Sang <zacharysang@gmail.com>
2017-04-11 19:27:14 -0400

    Require alt key press for pass to show

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
-var passFields = document.querySelectorAll('input[type="password"]');
+var keysPressed = false;
+document.body.addEventListener('keydown', function (ev) {
+  keysPressed = ((ev.altKey) || false);
+});
 
+document.addEventListener('keyup', function (ev) {
+  keysPressed = false;
+})
+
+var passFields = document.querySelectorAll('input[type="password"]');
 passFields.forEach(addListeners);
 
 //make password visible on hover
-function addListeners(element){
-  element.addEventListener('mouseover',function(ev){
-    ev.target.setAttribute('type','text');
+function addListeners(element) {
+  element.addEventListener('mouseover', function (ev) {
+    if (keysPressed) {
+      ev.target.setAttribute('type', 'text');
+    }
   });
-  
+
   //change it back on hover out
-  element.addEventListener('mouseout',function(ev){
-    ev.target.setAttribute('type','password')
+  element.addEventListener('mouseout', function (ev) {
+    ev.target.setAttribute('type', 'password')
   });
 }
-
--- options.js
+++ options.js
@@ -4,39 +4,39 @@ document.querySelector('#save').addEventListener('click', saveOptions);
 function saveOptions(e) {
   e.preventDefault();
 
-  if(document.querySelector('#isReqKeyPressed') == 'checked'){
+  if (document.querySelector('#isReqKeyPressed') == 'checked') {
     var statusEl = document.querySelector('#status');
 
     chrome.storage.sync.set({
-        pressKeys:document.querySelector('#pressKeys').value
-      },function(){
-         displaySucc(statusEl);
+      pressKeys: document.querySelector('#pressKeys').value
+    }, function () {
+      displaySucc(statusEl);
     });
   }
-  
+
 }
 
 function restoreOptions() {
-    var statusEl = document.querySelector('#status');
- 
-    chrome.storage.sync.get('pressKeys',function(res){
-        if(!runtime.lastErr){
-          document.querySelector('#pressKeys').value = res.pressKeys;
-          displaySucc(statusEl);
-        } else {
-          displayFail(statusEl);
-        }
-    });
+  var statusEl = document.querySelector('#status');
+
+  chrome.storage.sync.get('pressKeys', function (res) {
+    if (!runtime.lastErr) {
+      document.querySelector('#pressKeys').value = res.pressKeys;
+      displaySucc(statusEl);
+    } else {
+      displayFail(statusEl);
+    }
+  });
 
 }
 
-function displaySucc(el){
-   el.style.backgroundColor = '#81D274';
-   el.style.display = 'true';
-   el.innerText = "Saved";   
+function displaySucc(el) {
+  el.style.backgroundColor = '#81D274';
+  el.style.display = 'true';
+  el.innerText = "Saved";
 }
 
-function displayFail(el){
+function displayFail(el) {
   el.style.backgroundColor = '#D75353';
   el.style.display = 'true';
   el.innerText = 'Saving failed';
